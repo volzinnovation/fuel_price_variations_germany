a903106c7a2000531b20b6fadfd5975ed814dcc1
volzinnovation <39283695+volzinnovation@users.noreply.github.com>
2021-10-31 16:56:09 +0100

    5th attempt

--- install.r
+++ install.r
@@ -1,6 +1,6 @@
 options(tz="Europe/Berlin")
 Sys.setenv(TZ="Europe/Berlin")
-
+require(devtools)
 install_version("tzdb", version = "0.1.2", repos = "https://cloud.r-project.org/")
 # install.packages("tzdb", repos= "https://cloud.r-project.org/", dependencies = TRUE, lib="R_libs")
 install.packages("zoo", repos= "https://cloud.r-project.org/", dependencies = TRUE, lib="R_libs")
